7a33ddbf8422eb89ce52428a0383a94c3656d26a
nikolai <ngbaranov@gmail.com>
2026-05-22 13:43:51 +0300

    alembic

--- app/config.py
+++ app/config.py
@@ -10,6 +10,11 @@ class Settings(BaseSettings):
     db_host: str
     db_port: int = 5432
 
+    @property
+    def async_db_url(self) -> str:
+        """Construct the asynchronous database URL."""
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
--- app/db/database.py
+++ app/db/database.py
@@ -3,12 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
 
-# Строка подключения для PostgreSQl
-
-data_base_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 
 # Создаём Engine
-async_engine = create_async_engine(data_base_url, echo=True)
+async_engine = create_async_engine(settings.async_db_url, echo=True)
 
 # Настраиваем фабрику сеансов
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
