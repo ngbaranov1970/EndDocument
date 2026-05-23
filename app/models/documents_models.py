89e713df92d9522f2e28c13084ff074301603279
nikolai <ngbaranov@gmail.com>
2026-05-23 06:47:12 +0300

    update models

--- app/models/documents_models.py
+++ app/models/documents_models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.database import Base
@@ -5,6 +8,19 @@ from app.db.database import Base
 class Document(Base):
     __tablename__ = "documents"
 
+    # organization_name: str = Field(..., description="Название организации")
+    # user_name: str = Field(..., description="Имя пользователя")
+    # created_at: datetime = Field(..., description="Дата и время создания документа")
+    # start_at: datetime = Field(..., description="Дата и время начала работ")
+    # end_at: datetime = Field(..., description="Дата и время окончания работ")
+    # status: bool = Field(..., description="Статус документа (активный/неактивный)")
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    content: Mapped[str] = mapped_column(nullable=False)
+    user_name: Mapped[str] = mapped_column(nullable=False)
+    organization_name: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    start_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    end_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    status: Mapped[bool] = mapped_column(nullable=False, server_default=text("true"))
+
+
