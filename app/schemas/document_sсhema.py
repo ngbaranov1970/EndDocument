65cf359e12fed2d705205bf4b0d6a34ab8ede7f9
nikolai <ngbaranov@gmail.com>
2026-06-25 16:15:23 +0300

    init2

--- app/schemas/document_sсhema.py
+++ app/schemas/document_sсhema.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, Field, ConfigDict
 from datetime import date
 
 class DocumentCreate(BaseModel):
-    """Схема для создания документа, содержащая информацию об организации, имени пользователя, дате начала и окончания работ."""
-    organization_id: int = Field(..., gt=0, description="Идентификатор организации")
+    """Схема для создания документа, содержащая идентификатор организации, имя пользователя, дату начала и окончания работ."""
+    organization_id: int = Field(..., description="Идентификатор организации")
     user_name: str = Field(..., min_length=3, max_length=255, description="Имя пользователя (3-255 символов)")
-    start_at: date = Field(..., description="Дата начала работ")
-    end_at: date = Field(..., description="Дата окончания работ")
+    start_at: date = Field(..., description="Дата и время начала работ")
+    end_at: date = Field(..., description="Дата и время окончания работ")
 
 class Document(BaseModel):
     """Схема для отображения документа, содержащая информацию о названии организации, имени пользователя, дате и времени начала и окончания работ, а также статус документа."""
